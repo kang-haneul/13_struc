aa7400f4f4802fde54f9e78322a4e3abd0bdf828
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-11-26 15:25:46 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student{
+	
+int ID;
+char name[10];
+double grade;
+
+};
+
+
+int main(int argc, char *argv[]) {
+	
+		
+	struct student s1 = {24, "kang han eul", 4.3};
+	struct student s2 = {24, "kang han eul", 4.3};
+	struct student s3 = {24, "kang han eul", 4.3};
+	struct student s4 = {24, "kang han eul", 4.3};
+	struct student s5 = {24, "kang han eul", 4.3};
+	struct student s6 = {24,"kang han eul", 4.3};
+	struct student s7 = {24, "kang han eul", 4.3};
+	struct student s8 = {24, "kang han eul", 4.3};
+	struct student s9 = {24, "kang han eul", 4.3};
+	struct student s10 = {24, "kang han eul", 4.3};
+	
+	
+	return 0;
+}
